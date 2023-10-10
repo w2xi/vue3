6c9f5403b5636a7f313a9866ad6b7fc316c6b2c7
w2xi <43wangxi@gmail.com>
2023-10-10 19:01:10 +0800

    chore: tokenize

--- vuejs-design/chapter-15/parser/1.js
+++ vuejs-design/chapter-15/parser/1.js
@@ -1,15 +1,3 @@
-function isAlpha(char) {
-  return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
-}
-
-// <p>Vue</p>
-// =>
-// [
-//   { type: 'tag', name: 'p' },
-//   { type: 'text', content: 'Vue' },
-//   { type: 'tagEnd', name: 'p' },
-// ]
-
 const State = {
   initial: 1, // 初始化状态
   tagOpen: 2, // 标签开始状态
@@ -19,37 +7,93 @@ const State = {
   tagEndName: 6 // 结束标签名称状态
 }
 
+// 判断是否是字母
+function isAlpha(char) {
+  return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+
 function tokenize(str) {
   let currentState = State.initial
   let chars = ''
   const tokens = []
 
   while (str) {
-    const c = str[0]
-    if (c === '<') {
-      if (currentState === State.text) {
-        // 如果之前是 文本状态
-        tokens.push({ tag: 'text', content: chars })
-      }
-      // 进入 标签开始状态
-      currentState = State.tagOpen
-      str = str.slice(1)
-    } else if (isAlpha(c)) {
-      currentState = State.tagName
-      chars += c
-      str = str.slice(1)
-    } else if (c === '>') {
-      /***** 结束标签 ****/
-      if (currentState === State.tagName) {
-        // 如果之前是 标签名状态
-        tokens.push({ type: 'tag', name: chars })
-      }
-      // 迁移到初始状态
-      currentState = State.initial
-      // 重置
-      chars = ''
-    } else if (c === '/') {
-      currentState = State.tagEnd
+    const char = str[0]
+    switch (currentState) {
+      case State.initial:
+        if (char === '<') {
+          currentState = State.tagOpen
+        } else if (isAlpha(char)) {
+          currentState = State.text
+          chars += char
+        }
+        break
+      case State.tagOpen:
+        if (isAlpha(char)) {
+          currentState = State.tagName
+          chars += char
+        } else if (char === '/') {
+          currentState = State.tagEnd
+        }
+        break
+      case State.tagName:
+        if (isAlpha(char)) {
+          chars += char
+        } else if (char === '>') {
+          currentState = State.initial
+          // 收集 token
+          tokens.push({ type: 'tag', name: chars })
+          chars = ''
+        } else if (char === '/') {
+          currentState = State.tagEnd
+        }
+        break
+      case State.text:
+        if (isAlpha(char)) {
+          chars += char
+        } else if (char === '<') {
+          currentState = State.tagOpen
+          // 收集 token
+          tokens.push({ type: 'text', content: chars })
+          chars = ''
+        }
+        break
+      case State.tagEnd:
+        if (isAlpha(char)) {
+          currentState = State.tagEndName
+          chars += char
+        } else if (char === '>') {
+          // 处理半闭合标签 <img />
+          currentState = State.initial
+          tokens.push({ type: 'tag', name: chars })
+          chars = ''
+        }
+        break
+      case State.tagEndName:
+        if (isAlpha(char)) {
+          chars += char
+        } else if (char === '>') {
+          currentState = State.initial
+          tokens.push({ type: 'tagEnd', name: chars })
+          chars = ''
+        }
+        break
     }
+    str = str.slice(1)
   }
+
+  return tokens
 }
+
+const template = '<p>Vue</p>'
+const tokens = tokenize(template)
+
+console.log(tokens)
+
+// output:
+
+// [
+//   { type: 'tag', name: 'p' },
+//   { type: 'text', content: 'Vue' },
+//   { type: 'tagEnd', name: 'p' },
+// ]
